ff9bcaebb88c5800097d69c6fb7a55f657e4117e
jmiguels <jmiguels@mail.telepac.pt>
2023-05-23 21:20:03 +0100

    V0.02 16/03/2023
    V0.02 16/03/2023

--- CLInterface.ino
+++ CLInterface.ino
@@ -29,15 +29,75 @@ void CLInerface()
       Commandstr = Commandstr.substring(0,Commandstr.length()-1);
       CommandStrIsComplete=1;
     }
+    if ((Commandstr.length()>0 && (Commandstr[0]<63 || Commandstr[0]>90 || Commandstr[0]==64)) || (Commandstr.length()==3 && Commandstr[1] != 44) || (Commandstr.length()>3 && ((Commandstr[0] != 87 && Commandstr[0] != 67 && Commandstr[0] != 83 && Commandstr[0] != 77 && Commandstr[0] != 76) || Commandstr[1] != 44)))
+    {
+      Serial.println("Invalid command\r\n");
+      Serial.print(">");
+      Commandstr="";
+      CommandStrIsComplete=0;
+    }
   }
   else
   {
-    if (Commandstr.equals("")) DoNothing(); else
-    if (Commandstr.equals("?")||Commandstr.equals("help")||Commandstr.equals("Help")) Help(); else
-    if (Commandstr.equals("reset")) SystemReset(); else
-    if (Commandstr.substring(0,3).equals("set")) set(Commandstr); else    
-      Serial.println("Command Error\n");
-
+    switch (Commandstr[0])
+    {
+      //case 32: //Space
+      //  DoNothing();
+      case 63: //?
+        Help();
+        break;
+      case 72: //H
+        Help();
+        break;
+      case 82: //R
+        SystemReset();
+        break;
+      case 86: //V
+        DeviceVersion();
+        break;
+      case 84: //T
+        DeviceType();
+        break;
+      case 80: //P 
+        PowerSwitch(Commandstr);
+        break;
+      case 85: //U 
+        USB_ctrl(Commandstr);
+        break;
+      case 87: //W
+        KeyboardEmulator(Commandstr, 2500);
+        break;
+      case 67: //C
+        KeyboardEmulator(Commandstr, 2500);
+        break;
+      case 83: //S
+        SetServo(Commandstr);
+        break;
+      case 77: //M
+        RelayControl(Commandstr);
+        break;
+      case 76: //L
+        LedControl(Commandstr);
+        break;
+      case 66: //B
+        BuzzerControl();
+        break;
+      case 78: //N
+        KeyboardEmulatorLine(Commandstr);
+        break;
+      case 71: //G
+        GetLedColor();
+        break;
+      case 65: //A
+        OpenGabarit();
+        break;
+      case 70: //F
+        CloseGabarit();
+        break;
+      default:
+        Serial.println("Invalid command\r\n");
+        break;
+    }
   Serial.print(">");
   Commandstr="";
   CommandStrIsComplete=0;
@@ -55,59 +115,378 @@ void SystemReset()
   delay(2000); //Power down all the periferals during 2 second before powering UP. User for software reset  
 */ 
   ESP.restart();
+  Serial.print("r,ACK");
 }
 
 void DoNothing()
 {
 }
 
+void DeviceVersion()
+{
+  Serial.print("v,ACK,"); 
+  Serial.println(FirmwareVersion);
+}
+
+void DeviceType()
+{
+  Serial.print("t,ACK,"); 
+  Serial.println(DeviceTypeStr);
+}
+
+void PowerSwitch(String Commandstr)
+{
+  if (Commandstr.length()!=3)
+  {
+    Serial.println("p,NACK,Invalid command");
+  }
+  else if (Commandstr[2] == 48 || Commandstr[2] == 49)
+  {
+    if (Commandstr[2] == 48)
+    {
+      Relay(3, false);
+      Serial.println("p,ACK,0");
+    }
+    else
+    {
+      Relay(3, true);
+      Serial.println("p,ACK,1");
+    }
+  }
+  else
+  {
+    Serial.print("p,NACK,Invalid position");
+  }
+}
+
+void USB_ctrl(String Commandstr)
+{
+  if (Commandstr.length()!=3)
+  {
+    Serial.println("u,NACK,Invalid command");
+  }
+  else if (Commandstr[2] == 48 || Commandstr[2] == 49)
+  {
+    if (Commandstr[2] == 48)
+    {
+      digitalWrite(USB,LOW);
+      Serial.println("u,ACK,0");
+    }
+    else
+    {
+      digitalWrite(USB,HIGH);
+      Serial.println("u,ACK,1");
+    }
+  }
+  else
+  {
+    Serial.print("p,NACK,Invalid position");
+  }
+}
+
+void KeyboardEmulator(String Commandstr, unsigned long timeout)
+{
+  char dec_char;
+  String expectedstr;
+  if (Commandstr[0] == 87)
+  {
+    if (Commandstr.length()<3 and Commandstr[1] != 44)
+    {
+      Serial.println("w,NACK,Invalid command");
+    }
+    else if (Serial2)
+    {
+      String command = Commandstr + "\r\n";
+      
+      Serial2.write(command.c_str());
+      unsigned long start_time = millis();
+      while(true)
+      {
+        if (millis()-start_time>timeout)
+        {
+          Serial.println("w,NACK,Timeout exceded");
+          break;
+        }
+        if (Serial2.available()) 
+        {
+          dec_char = Serial2.read();
+          if (dec_char==10) //if newline
+          {  
+            expectedstr = "w,ACK," + Commandstr.substring(2,Commandstr.length());
+            if (finalstr == expectedstr)
+            {
+              Serial.println(expectedstr);
+            }
+            else
+            {
+              Serial.print("w,NACK,Something went wrong sending Keyboard key");
+              Serial.println(finalstr);
+            }
+            finalstr="";
+            break;
+          }
+          else if (dec_char!=13)
+          {
+            finalstr += (char)dec_char;
+          }  
+        } 
+      }
+    }
+    else
+    {
+      Serial.println("w,NACK,Impossible to communicate with Keyboard Emulator");
+    }
+  }
+  else
+  {
+    if (Commandstr.length()<3)
+    {
+      Serial.println("c,NACK,Invalid command");
+    }
+    else if (Serial2)
+    {
+      String command = Commandstr + "\r\n";
+      
+      Serial2.write(command.c_str());
+      unsigned long start_time = millis();
+      while(true)
+      {
+        if (millis()-start_time>timeout)
+        {
+          Serial.println("c,NACK,Timeout exceded");
+          break;
+        }
+        if (Serial2.available()) 
+        {
+          dec_char = Serial2.read();
+          if (dec_char==10) //if newline
+          {  
+            expectedstr = "c,ACK," + Commandstr.substring(2,Commandstr.length());
+            if (finalstr == expectedstr)
+            {
+              Serial.println(expectedstr);
+            }
+            else
+            {
+              Serial.print("c,NACK,Something went wrong sending Keyboard key - ");
+              Serial.println(finalstr);
+            }
+            finalstr="";
+            break;
+          }
+          else if (dec_char!=13)
+          {
+            finalstr += (char)dec_char;
+          }  
+        } 
+      }
+    }
+    else
+    {
+      Serial.println("c,NACK,Impossible to communicate with Keyboard Emulator");
+    }
+  }
+}
+
+void KeyboardEmulatorLine(String Commandstr)
+{
+  
+  if (Commandstr.length()!=3)
+  {
+    Serial.println("n,NACK,Invalid command");
+  }
+  else if (Commandstr[2] == 48 || Commandstr[2] == 49)
+  { 
+    if (Serial2)
+    {
+      String command = "L," + String(Commandstr[2]) + "\r\n";
+      Serial2.write(command.c_str());
+      delay(100);
+      while (Serial2.available()){
+          char dec_char = Serial2.read();
+          if (dec_char==10) //if newline
+          {  
+            String expectedstr = "l,ACK," + String(Commandstr[2]);
+            if (finalstr == expectedstr)
+            {
+              Serial.println("n,ACK," + String(Commandstr[2]));
+            }
+            else
+            {
+              Serial.println("n,NACK,Something went wrong configuring line feed");
+            }
+            finalstr="";
+            break;
+          }
+          else if (dec_char!=13)
+          {
+            finalstr += (char)dec_char;
+          }  
+        }
+    }
+  }
+  else
+  {
+    Serial.print("n,NACK,Sate must be '0' or '1'");
+  }
+}
+
+void SetServo(String Commandstr)
+{
+  uint8_t i = Commandstr.length()-1;
+  if (Commandstr.length()<5 || Commandstr[3]!=44)
+  {
+    Serial.println("s,NACK,Invalid command");
+    return;
+  }
+  else if (Commandstr[2]<49 || Commandstr[2]>55)
+  {
+    Serial.println("s,NACK,Invalid Servo. Must be between 1 and 7");
+    return;
+  }
+  while(Commandstr[i]!=44)
+  {
+    if (Commandstr[i]<48 || Commandstr[i]>57)
+    {
+      Serial.println("s,NACK,Invalid degree");
+      return;
+    }
+    i--;
+  }
+  int degree = Commandstr.substring(4,Commandstr.length()).toInt();
+  if (Commandstr[2]==49) servo_1.write(degree); else 
+  if (Commandstr[2]==50) servo_2.write(degree); else 
+  if (Commandstr[2]==51) servo_3.write(degree); else 
+  if (Commandstr[2]==52) servo_4.write(degree); else 
+  if (Commandstr[2]==53) servo_5.write(degree); else 
+  if (Commandstr[2]==54) servo_6.write(degree); else 
+  if (Commandstr[2]==55 && Servo7Active) servo_7.write(degree);
+  Serial.print("s,ACK,");
+  String servo (Commandstr[2]);
+  String state (Commandstr.substring(4,Commandstr.length()));
+  Serial.print(servo);
+  Serial.print(",");
+  Serial.println(state);
+}
+
+void RelayControl(String Commandstr)
+{
+  if (Commandstr.length()<5 || Commandstr[3]!=44)
+  {
+    Serial.println("l,NACK,Invalid command");
+  }
+  else if (Commandstr[2]!=49 && Commandstr[2]!=50)
+  {
+    Serial.println("l,NACK,Invalid relay. Must be 1 or 2");
+  }
+  else if (Commandstr[4]!=48 && Commandstr[4]!=49)
+  {
+    Serial.println("l,NACK,Invalid state. Must be 0 or 1");
+  }
+  else
+  {
+    String relay (Commandstr[2]);
+    String state (Commandstr[4]);
+    if (Commandstr[4]!=48) Relay(relay.toInt(), true);else Relay(relay.toInt(), false);
+    Serial.print("l,ACK,");
+    Serial.print(relay);
+    Serial.print(",");
+    Serial.println(relay);
+  }
+}
+
+void LedControl(String Commandstr)
+{
+  if (Commandstr.length()<9 || Commandstr[3]!=44)
+  {
+    Serial.println("l,NACK,Invalid command");
+  }
+  else if (Commandstr[4]!=40 || Commandstr[Commandstr.length()-1]!=41)
+  {
+    Serial.println("l,NACK,Color definition must be between '(' and ')'");
+  }
+  else
+  {
+    uint8_t count = 0;
+    for (uint8_t i=0; i<Commandstr.length(); i++)
+      if (Commandstr[i] == ',') count++;
+    if (count!=4)Serial.println("l,NACK,Invalid command");else
+    {
+      String idStr = Commandstr.substring(2,3);
+      String redStr = Commandstr.substring(5,Commandstr.indexOf(',',5));
+      String greenStr = Commandstr.substring(Commandstr.indexOf(',',5)+1,Commandstr.indexOf(',',Commandstr.indexOf(',',5)+1));
+      String blueStr = Commandstr.substring(Commandstr.indexOf(',', Commandstr.indexOf(',', 5) + 1) + 1, Commandstr.indexOf(')'));
+      int idInt = idStr.toInt();
+      int redInt = redStr.toInt();
+      int greenInt = greenStr.toInt();
+      int blueInt = blueStr.toInt();
+      uint8_t id = static_cast<uint8_t>(idInt);
+      uint8_t red = static_cast<uint8_t>(redInt);
+      uint8_t green = static_cast<uint8_t>(greenInt);
+      uint8_t blue = static_cast<uint8_t>(blueInt);
+
+      if (redInt>255 || greenInt>255 || blueInt>255) Serial.println("l,NACK,Invalid RGB values. Must be between 0 and 255"); else
+      {
+        LedCtrl(id, red, green, blue);
+      Serial.print("l,ACK,");
+      Serial.print(id);
+      Serial.print(",(");
+      Serial.print(red);
+      Serial.print(",");
+      Serial.print(green);
+      Serial.print(",");
+      Serial.print(blue);
+      Serial.println(")"); 
+      }
+    }
+  }
+}
+
+void BuzzerControl()
+{
+  PlayBuzzer();
+  Serial.println("b,ACK,");
+}
+
+void GetLedColor()
+{
+  GetColor();
+}
+
+void CloseGabarit()
+{
+  if(Close()==0) 
+    Serial.println("f,ACK,");
+  else
+    Serial.println("f,NACK,Emergency");
+}
+
+void OpenGabarit()
+{
+  Open();
+  Serial.println("a,ACK,");;
+}
+
+
 void Help()
 {
   String HelpString=""; 
   Serial.println("This is HELP");
-  HelpString+="Help            - Use ?, help or Help to get help\n\r";
-  HelpString+="Reset           - Reset the unit\n\r";
-  HelpString+="version         - Returns type of device and version\n\r";  
-  HelpString+="type            - Returns type of device\n\r";    
-  HelpString+="set \"servoN\"  - set the servoN angle (between 0 and 180 degrees).\n\r";
-  HelpString+="                  N is a number between 1 and 7\n\r";
-  HelpString+="                  Example to set the servo2 angle to 20 degrees: set \"servo2\" \"20\"\n\r";
+  HelpString+="H                     - Use ? or H to get help\n\r";
+  HelpString+="R                     - Reset the unit\n\r";
+  HelpString+="T                     - Get device type\n\r";
+  HelpString+="V                     - Get device firmware version\n\r";  
+  HelpString+="P,<state>             - Set Power Switch state ('0'- Off and '1' - On).Ex to power on: P,1\n\r";
+  HelpString+="U,<state>             - Set USB Switch state ('0'- Off and '1' - On).Ex to switch on: U,1\n\r";
+  HelpString+="W,<key>               - Send <key> through keyboard\n\r";
+  HelpString+="N,<state>              - Configure Remote Emulator line feed('0' - Off and '1' - On). Ex to activate line feed: L,1\n\r";
+  HelpString+="C,<key>               - Send control characters(hexadecimal) through keyboard. Ex for right arrow.:C,57\n\r";
+  HelpString+="S,<servo>,<degres>    - Set servo to certain degree. Ex set servo 1 to 180 degrees: S,1,180\n\r";
+  HelpString+="M,<relay>,<state>     - Set relay ('1' or '2') state ('0' - Off and '1' - On).Ex to close relay 1: M,1,1\n\r";
+  HelpString+="L,<id>,<(R,G,B)>      - Set Led's Color(RGB). Ex change led 1 to white: L,1,(255,255,255)\n\r";
+  HelpString+="B                     - Play buzzer during 1s\n\r";
+  HelpString+="G                     - Get color from Led\n\r";
+  HelpString+="A                     - Open Gabarit\n\r";
+  HelpString+="F                     - Close Gabarit\n\r";
   Serial.println(HelpString);
-}
-
-/**************************************
- **************************************/
-void GetVersion()
-{
-  Serial.println(VersionStr);
-}
-
-/**************************************
- **************************************/
-void GetType()
-{
-  Serial.println(DeviceTypeStr);
-}
-
-void set(String InputString)
-{
-  String Parameter;
-  String Value;
-  
-  Parameter=InputString.substring(5,InputString.indexOf("\"",6));
-  Value=InputString.substring(5+Parameter.length()+3,InputString.indexOf("\"",5+Parameter.length()+3));
-
-  if (Parameter.equals("servo1")) servo_1.write(Value.toInt()); else 
-  if (Parameter.equals("servo2")) servo_2.write(Value.toInt()); else 
-  if (Parameter.equals("servo3")) servo_3.write(Value.toInt()); else 
-  if (Parameter.equals("servo4")) servo_4.write(Value.toInt()); else 
-  if (Parameter.equals("servo5")) servo_5.write(Value.toInt()); else 
-  if (Parameter.equals("servo6")) servo_6.write(Value.toInt()); else 
-  if (Parameter.equals("servo7") && Servo7Active) servo_7.write(Value.toInt()); else 
-    
-  {  
-    Serial.println("Set Command Error\n");
-    return;
-  }  
-  Serial.println("OK");
 }
--- CtrlGabarit.ino
+++ CtrlGabarit.ino
@@ -6,10 +6,12 @@ JMSantos 2023
 #include "Arduino.h"
 #include <Servo.h> // https://randomnerdtutorials.com/esp32-servo-motor-web-server-arduino-ide/
                    // Biblioteca "ServoESP32"de Jaroslav Paral
+#include <Wire.h>  // Necessário para utilizar a biblioteca SparkFun_APDS9960.h
+#include <SparkFun_APDS9960.h> //https://github.com/sparkfun/APDS-9960_RGB_and_Gesture_Sensor
 
-String FirmwareVersion="V0.02 16/03/2023";
-String DeviceTypeStr="GABARIT_CTRL";
-String VersionStr= DeviceTypeStr + " - " + FirmwareVersion;
+String DeviceTypeStr="GABARIT_CTRL";                              //Device name
+String FirmwareVersion="V0.02 - 16/03/2023";                      //Firmware Version
+String VersionStr= DeviceTypeStr + " - " + FirmwareVersion; 
 
 //############# PINs definitions #############
 
@@ -21,7 +23,8 @@ String VersionStr= DeviceTypeStr + " - " + FirmwareVersion;
 #define Buzzer      4  
 #define Relay1     14  
 #define Relay2     27  
-#define Relay3     25 // Multiplexado com o Servo7 e com o solenoide 6 
+#define Relay3     25 // Multiplexado com o Servo7 e com o solenoide 6  
+#define USB        13
 #define Solenoid1  19  
 #define Solenoid2  18  
 #define Solenoid3   5  
@@ -38,8 +41,15 @@ String VersionStr= DeviceTypeStr + " - " + FirmwareVersion;
 #define Servo7     25
 
 #define D32IN      32 //utilizado para configurar o GND-Servo7 ou o ABERTO-Relé3
-#define D35IN      35 
-#define D34IN      34 
+
+#define RXD2 16
+#define TXD2 17  
+
+hw_timer_t * timer = NULL; //******** Timer Interrupt definitions
+
+#define RelayPulseDuration        500000   //pulse duration for the relay activation in micro seconds
+#define InterruptActivationDelay  1000000  //Delay to activate interrupts again after a command
+int LastCommandWasOpen = true;
 //###################################################
 
 Servo servo_1;  // create servo object to control a servo
@@ -50,12 +60,12 @@ Servo servo_5;  // create servo object to control a servo
 Servo servo_6;  // create servo object to control a servo
 Servo servo_7;  // create servo object to control a servo
 
-
 bool Servo7Active=false;
 
 //######### CLInterface #############################
 String Commandstr = "";
 int CommandStrIsComplete=0;
+String finalstr="";
 //###################################################
 
 //############# Adressable LEDS WS2812B #############
@@ -67,14 +77,66 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS,LED_PIN, NEO_GRB + NEO_KHZ
 //############################## BUZZER
 #define PWM_resolution    8  //number of bits used to define the duty cycle (can be from 8 to 16)
 #define PWM3_Ch           2  //Definição do canal de PWM
-#define PWM4_Ch           8  //Definição do canal de PWM
+#define PWM4_Ch           3  //Definição do canal de PWM
 //###################################################
+
+//***** Color sensor SparkFun_APDS9960
+  SparkFun_APDS9960 apds = SparkFun_APDS9960();
+  uint16_t ambient_light = 0;
+  uint16_t red_light = 0;
+  uint16_t green_light = 0;
+  uint16_t blue_light = 0;
+//***********************************
+
+void IRAM_ATTR InterruptToOpen()
+{
+  Open();
+}
+
+void IRAM_ATTR InterruptToClose()
+{  
+  if(Close()==1)
+  {
+    Serial.println("Emergency pressed");
+  }
+}
+
+void TimerInterruptToFinishPulse()
+{
+  timerEnd(timer); //Destroi o timer
+  digitalWrite(Relay1,LOW);
+  digitalWrite(Relay2,LOW);
+  //Timer setup
+  timer = timerBegin(0, 80, true); // Timer 0 programado comum prescale de 80 (o relógio é de 80MHz). countup (true) ou countdown (false)
+  timerAttachInterrupt(timer, &ActivateInterruptsAgain, true); //Definição da rotina de serviço ao interrupt. edge (true) or level (false)  
+  timerAlarmWrite(timer, InterruptActivationDelay, true); //configurar o timer interrupt TimerInterruptToFinishPulse() para daqui a 1s
+  timerAlarmEnable(timer); 
+}
+
+void ActivateInterruptsAgain()
+{
+  timerEnd(timer); //Destroi o timer
+  EnableButtonInterrupts();
+}
+
+void EnableButtonInterrupts()
+{
+  attachInterrupt(BUTTON1, InterruptToOpen, FALLING);
+  attachInterrupt(BUTTON2, InterruptToClose, FALLING);   
+}
+
+void DetachButtonInterrupts()
+{
+  detachInterrupt(BUTTON1);
+  detachInterrupt(BUTTON2);  
+}
 
 void setup()
 {
   int n;
   
-	Serial.begin(115200);
+	Serial.begin(115200);                                                                                                   
+  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
 
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUTTON1, INPUT_PULLUP);
@@ -89,15 +151,15 @@ void setup()
   pinMode(Solenoid5, OUTPUT);
   pinMode(Solenoid6, OUTPUT);
   pinMode(D32IN, INPUT_PULLUP);
-  pinMode(D34IN, INPUT);
-  pinMode(D35IN, INPUT);
 
+
+  init_APDS9960();
+  
   if(digitalRead(D32IN)==LOW) 
   {
     Serial.println("Configurar servo 7");
     Servo7Active=true;  
   }
-
 
   servo_1.attach(Servo1);  // attaches the servo on pin Servo1 to the servo object
   servo_2.attach(Servo2);  // attaches the servo on pin Servo2 to the servo object
@@ -105,13 +167,14 @@ void setup()
   servo_4.attach(Servo4);  // attaches the servo on pin Servo4 to the servo object
   servo_5.attach(Servo5);  // attaches the servo on pin Servo5 to the servo object
   servo_6.attach(Servo6);  // attaches the servo on pin Servo6 to the servo object
+
   if(Servo7Active) 
   {
     Serial.println("Configurar servo 7");
     servo_7.attach(Servo7);  // Não pode ser utilizado se quisermos utilizar o Relé3
   }
   
-  ledcAttachPin(Buzzer, PWM4_Ch); //Assign a PWM channel for the Buzzer  
+  ledcAttachPin(Buzzer, PWM3_Ch); //Assign a PWM channel for the Buzzer  
   
   for(n=0;n<NUM_LEDS;n++)
     LedCtrl(n, 0,255,0);
@@ -125,16 +188,69 @@ void setup()
   
   delay(1000);
   for(n=0;n<NUM_LEDS;n++)
+    LedCtrl(n, 0,0,255);
+
+  delay(1000);
+  for(n=0;n<NUM_LEDS;n++)
     LedCtrl(n, 0,0,0);
+
+  EnableButtonInterrupts();
+  
+  Serial.print("Buton1: ");
+  Serial.print(digitalRead(BUTTON1));
+  Serial.print(" Buton2: ");
+  Serial.print(digitalRead(BUTTON2));
+  Serial.println();
+  Serial2.println();  //Enviado comando para arduino para limpar buffer.
 }
 
 void loop()
 {
-  CLInerface(); // Client Line Interface processing over the serial port  
-  /*
-  Relay(3, true);
-  delay(1000);
-  Relay(3, false);
-  delay(1000);
-  */
+  CLInerface(); // Client Line Interface processing over the serial port
+  if((!LastCommandWasOpen)&&(digitalRead(BUTTON1)==LOW)) //ALTERAR PARA HIGH SE EMERGENGY BUTTON FOR NC
+  {
+    Open();
+    LastCommandWasOpen=true;
+  }
+}
+
+void Open()
+{
+  DetachButtonInterrupts();
+  //Timer setup
+  timer = timerBegin(0, 80, true); // Timer 0 programado comum prescale de 80 (o relógio é de 80MHz). countup (true) ou countdown (false)
+  timerAttachInterrupt(timer, &TimerInterruptToFinishPulse, true); //Definição da rotina de serviço ao interrupt. edge (true) or level (false)  
+  timerAlarmWrite(timer, RelayPulseDuration, true); //configurar o timer interrupt TimerInterruptToFinishPulse() para daqui a 0.5s
+  timerAlarmEnable(timer); 
+  
+  digitalWrite(Relay2,LOW);  //desativa primeiro o rele1
+  digitalWrite(Relay1,HIGH);  //ativa o rele2
+  LastCommandWasOpen=true;
+}
+
+int Close()
+{
+  DetachButtonInterrupts();
+  if(digitalRead(BUTTON1)==HIGH) //Emergency Button not pressed //ALTERAR PARA LOW SE EMERGENGY BUTTON FOR NC
+  {
+    //Timer setup
+    timer = timerBegin(0, 80, true); // Timer 0 programado comum prescale de 80 (o relógio é de 80MHz). countup (true) ou countdown (false)
+    timerAttachInterrupt(timer, &TimerInterruptToFinishPulse, true); //Definição da rotina de serviço ao interrupt. edge (true) or level (false)  
+    timerAlarmWrite(timer, RelayPulseDuration, true); //configurar o timer interrupt TimerInterruptToFinishPulse() para daqui a 0.5s
+    timerAlarmEnable(timer); 
+  
+    digitalWrite(Relay1,LOW);   //desativa primeiro o rele1
+    digitalWrite(Relay2,HIGH);  //ativa o rele2
+    LastCommandWasOpen=false;
+    return(0);
+  }
+  else
+  {
+    //Timer setup
+    timer = timerBegin(0, 80, true); // Timer 0 programado comum prescale de 80 (o relógio é de 80MHz). countup (true) ou countdown (false)
+    timerAttachInterrupt(timer, &ActivateInterruptsAgain, true); //Definição da rotina de serviço ao interrupt. edge (true) or level (false)  
+    timerAlarmWrite(timer, InterruptActivationDelay, true); //configurar o timer interrupt TimerInterruptToFinishPulse() para daqui a 1s
+    timerAlarmEnable(timer); 
+    return(1);
+  }
 }
